4f49bcedea898bd17a089193e8ef96f2e7bc9074
kmg8280 <kkm8280@naver.com>
2016-11-13 00:00:01 +0900

    Update ListOfUniqueNumbers.cpp

--- ListOfUniqueNumbers.cpp
+++ ListOfUniqueNumbers.cpp
@@ -5,7 +5,7 @@ int check[100001];
 int main()
 {
     int n;
-    memset(check, -1, sizeof(check));
+    memset(check, -1, sizeof(check)); // -1로 초기화한다. -1이면 처음 등장하는 수이고 -1이 아니면 중복되는 수 일 것이다.
     scanf("%d", &n);
  
     int arr[100000];
@@ -18,15 +18,15 @@ int main()
     {
         if (check[arr[i]] != -1) // 2번 이상.
         {
-            if (check[arr[i]]>closer)
+            if (check[arr[i]]>closer) // 클로저 값을 가장 가까운 중복되는 수 index로 설정한다.
                 closer = check[arr[i]];
             ans += (long long)(i - closer);
-            check[arr[i]] = i;
+            check[arr[i]] = i; // 해당 수의 가장 최근 index 값을 저장한다.
         }
         else
         {
             ans += (long long)(i - closer);
-            check [arr[i]] = i;
+            check [arr[i]] = i; // 해당 수의 가장 최근 index 값을 저장한다.
         }
     }
     printf("%lld\n", ans);
